d1ed817b9244af7855a88d861c20a00a3700f505
420-35 <romankir35@mail.ru>
2025-10-06 17:17:47 +0300

    fix: add NDK version and CI workflow with DI generation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "ru.legalassistant.ru_legal_assistant"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    namespace = "ru.legalassistant.app"
+    compileSdk = 34
+    ndkVersion = 27.0.12077973
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ru.legalassistant.ru_legal_assistant"
+        applicationId = "ru.legalassistant.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
